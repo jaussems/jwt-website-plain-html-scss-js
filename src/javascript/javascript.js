584694c1c425186f207c0e76175ec52178261d01
jaussems <jannesaussems@gmail.com>
2022-11-12 17:31:24 +0100

    cleaned up js file

--- src/javascript/javascript.js
+++ src/javascript/javascript.js
@@ -15,25 +15,7 @@ const darkModeToggle = () => {
 
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
-    
-    
-    // isDark = !isDark;
-    
-    // isDark ? setDarkTheme() : setLightTheme();
- // If document have an attribute data-theme with value dark, set attribute to light, else set to dark.
-
 
 }
 
 
-// const  setDarkTheme = () => {
-//     const body = document.body;
-//     body.style.backgroundColor = 'black';
-//     body.style.color = 'white';
-// }
-
-// const  setLightTheme = () => {
-//     const body = document.body;
-//     body.style.backgroundColor = 'white';
-//     body.style.color = 'black';
-// }
