b3d5aaf3a3638ad78b6028aba2c3d9f8e530ffbe
jaussems <jannesaussems@gmail.com>
2022-11-12 17:26:15 +0100

    messing around with dark mode

--- src/javascript/javascript.js
+++ src/javascript/javascript.js
@@ -1,8 +1,39 @@
-let isDarkMode = false;
-let elementBody = document.querySelector('inner-div');
-const darkModeToggle = () => {
+let isDark = false;
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
 
-    console.log(elementBody);
-    isDarkMode = !isDarkMode;
-    isDarkMode ? elementBody.className = 'dark-mode' :  elementBody.className ='light-mode'
+const darkModeToggle = () => {
+    let style = {};
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+    
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+    
+    
+    // isDark = !isDark;
+    
+    // isDark ? setDarkTheme() : setLightTheme();
+ // If document have an attribute data-theme with value dark, set attribute to light, else set to dark.
+
+
 }
+
+
+// const  setDarkTheme = () => {
+//     const body = document.body;
+//     body.style.backgroundColor = 'black';
+//     body.style.color = 'white';
+// }
+
+// const  setLightTheme = () => {
+//     const body = document.body;
+//     body.style.backgroundColor = 'white';
+//     body.style.color = 'black';
+// }
