f0002cd21fd6bfaf00790f9ceaed3c923d154e9b
jaussems <jannesaussems@gmail.com>
2023-04-30 16:45:02 +0200

    returns error message when user forgets to put email or text in

--- src/javascript/mail_services.js
+++ src/javascript/mail_services.js
@@ -2,111 +2,64 @@
 const form_id_js = "modal_form";
 
 const data_js = {
-    "access_token": "cbccll26waqohl0q9i71mgur"
+    access_token: "cbccll26waqohl0q9i71mgur",
 };
 
 const sendButton = document.getElementById("js_send");
 
 sendButton.addEventListener("click", function (e) {
-    //console.log('testing e ');
     e.preventDefault();
 });
 
-function js_send(event) {
-    sendButton.value = 'Sending…';
-    sendButton.disabled = true;
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        } else
-            if (request.readyState == 4) {
-                js_onError(request.response);
-            }
-    };
 
-    const subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    const message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    data_js['subject'] = subject;
-    data_js['text'] = message;
-    const params = toParams(data_js);
-
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    request.send(params);
-    event.preventDefault();
-    evt.stopPropagation();
-
-    //return false;
-}
 async function sendForm(e) {
-
+    const errorMessage = document.getElementById("error_message");
     const request = new XMLHttpRequest();
-
-
-    const subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    const message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    data_js['subject'] = subject;
-    data_js['text'] = message;
+    const subject = document.querySelector(
+        "#" + form_id_js + " [name='subject']"
+    ).value;
+    const message = document.querySelector(
+        "#" + form_id_js + " [name='text']"
+    ).value;
+    data_js["subject"] = subject;
+    data_js["text"] = message;
+    if (!subject || !message) {
+        errorMessage.classList.add("modal_message--danger");
+        errorMessage.classList.add("bounce");
+        errorMessage.textContent = "Please fill in your email / message 👎";
+        return;
+    }
     const params = toParams(data_js);
-
     request.open("POST", "https://postmail.invotes.com/send", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
     request.send(params);
-    this.errorHandler(request, subject.value, message.value);
-    // if (request.status !== 200) {
-    //     errorMessage.textContent = 'Failed to send your message';
-    // }
-
-    // if (!subject.value || !message.value) {
-    //     errorMessage.textContent = 'Please fill in your email / message';
-    // }
-
-    console.log(request);
-
+    this.errorHandler(request, params);
+    document.querySelector("#" + form_id_js + " [name='subject']").value = "";
+    document.querySelector("#" + form_id_js + " [name='text']").value = "";
     e.preventDefault();
     e.stopImmediatePropagation();
 
     return false;
 }
 
-
 function toParams(data_js) {
     const form_data = [];
     for (const key in data_js) {
-        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+        form_data.push(
+            encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key])
+        );
     }
 
     return form_data.join("&");
 }
 
 function errorHandler(request) {
-    const errorMessage = document.getElementById('error_message');
-    const subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    const message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    if (request.status === 200) {
-        errorMessage.classList.add('modal_message--succes');
-        errorMessage.classList.add('bounce');
-        errorMessage.textContent = 'You have succesfully send your message 👍';
+    const errorMessage = document.getElementById("error_message");
+    //needs redesign in future, when mailing service is build.
 
+    if (request) {
+        errorMessage.classList.add("modal_message--succes");
+        errorMessage.classList.add("bounce");
+        errorMessage.textContent = "You have succesfully send your message 👍";
     }
-    if (request.status !== 200) {
-        errorMessage.classList.add('modal_message--danger');
-        errorMessage.classList.add('bounce');
-        errorMessage.textContent = `Failed to send your message👎`;
-
-    }
-
-    if (!subject.value || !message.value) {
-        errorMessage.classList.add('modal_message--danger');
-        errorMessage.classList.add('bounce');
-        errorMessage.textContent = 'Please fill in your email / message 👎';
-    }
-
-    setTimeout(() => {
-        errorMessage.style.display = 'none';
-    }, 5000)
 }
-
